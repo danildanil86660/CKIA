63819e2f95477d9056a78fbc83a8d842f1afc058
danildanil86660 <dan0712@yandex.ru>
2020-03-22 00:59:04 +0300

    Create dev

---
+++ OPCClient/client.py
@@ -0,0 +1,20 @@
+from opcua import Client
+
+
+class OPCClient:
+
+    def __init__(self, url="opc.tcp://127.0.0.1:48402/freeopcua/server/"):
+        self.url = url
+        self.client = Client(self.url)
+
+    def get_value(self, count):
+        num = 2
+        list_value = list()
+        while num < count + 2:
+            list_value.append(self.client.get_node(f'ns=2; i={num}').get_value())
+            num += 1
+        return list_value
+
+    def run(self):
+        self.client.connect()
+        print(f'Client connected to {self.url}')
--- OPCServer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpcserverConfig(AppConfig):
-    name = 'OPCServer'
